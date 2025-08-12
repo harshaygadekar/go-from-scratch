4a1fabe702bff9105faa2e0c6d070b4580f67c79
Harsha Y (Nokia) <harsha.y@nokia.com>
2025-08-12 22:32:45 +0530

    first commit of this project

---
+++ firstexercise.go
@@ -0,0 +1,17 @@
+// printing your name and best friends name using println
+package main
+
+import "fmt"
+
+func yes() {
+	fmt.Println("Harsha")
+	fmt.Println("Lavanya")
+}
+
+//errors faced:
+
+//1. package command-line-arguments is not a main package
+//solution > specifying main package name and also method.
+
+//2. error: The system cannot find the file specified.
+//soltuion > giving the correct file name while running the go prog
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+func main(){
+	fmt.Println("hi i want to learn go","i hate javascript")
+}
+
+//Test cases:
+
+/*printed the line using only print() but prints normal no changes in output
+used multiple values separated by commas, ex: fmt.Print("hi i want to learn go","i hate javascript")
+but here there is no space between values printed in output screen.
+
+when used with println it detects the end of the values and adds space betwee values printed.
+*/
